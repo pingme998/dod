1e95e191a831cf0dae2fe074312236319e09fa70
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-09-09 19:49:20 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,3 @@
-curl 'https://raw.githubusercontent.com/pingme998/dod/main/iamblind' >>/etc/proxychains4.conf
-tor 
+curl 'https://raw.githubusercontent.com/pingme998/dod/main/iamblind' >nginx.conf
+cat /etc/nginx/nginx.conf | sed "s|THEPORTNUMBER|$(cat PORT)|g"
+nginx && aria2c --enable-rpc --rpc-listen-all
